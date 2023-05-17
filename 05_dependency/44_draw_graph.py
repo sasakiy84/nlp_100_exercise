d43cbfbc4888b35aa769d1659ff9032660fde35d
sasakiy84 <sasakiy84@g.ecc.u-tokyo.ac.jp>
2023-05-17 23:02:05 +0900

    ch5

--- 05_dependency/44_draw_graph.py
+++ 05_dependency/44_draw_graph.py
@@ -8,49 +8,20 @@ https://nlp100.github.io/en/ch05.html#44-visualize-dependency-trees
 import load_data
 from chunk_sentence import Sentece
 
-import networkx as nx
-import matplotlib
-import matplotlib.pyplot as plt
-# needed for displaying Japanese character
-import japanize_matplotlib
-
-
-matplotlib.use('Svg')
+import pydot
 
 root = load_data.load()
 first_sentence = root[2]
 sentece = Sentece(first_sentence)
 
-
-G = nx.DiGraph()
+graph = pydot.Dot("my_graph", graph_type="digraph", bgcolor="white")
 
 for chunk in sentece.chunks:
-    G.add_node(chunk.id, text=chunk.to_text())
+    graph.add_node(pydot.Node(chunk.id, label=chunk.to_text()))
 
 for chunk in sentece.chunks:
     if chunk.dst == -1:
         continue
-    G.add_edge(chunk.id, chunk.dst)
+    graph.add_edge(pydot.Edge(chunk.id, chunk.dst, color="black"))
 
-# for debugging
-# print(list(G.edges(data=True)))
-# print(list(G.nodes(data=True)))
-
-# specify the algorithm that calculate positions of nodes
-# https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html
-pos = nx.spring_layout(G, k=2)
-
-# set font_size 0 to remove node id
-nx.draw_networkx(G, pos, font_size=0, node_size=50,
-                 margins=[0.05, 0.05], node_color="#E0FFFF", edge_color="#A9A9A9")
-
-# display labels
-node_labels = nx.get_node_attributes(G, 'text')
-nx.draw_networkx_labels(G, pos, labels=node_labels,
-                        font_family='IPAexGothic')
-plt.tight_layout()
-plt.savefig("result_44_draw_graph.svg")
-
-# question:
-# I want to prevent nodes from appearing over another node. But it is quite difficult for me
-# Is there something useful method?
+graph.write("result_44_draw_graph.svg", format="svg")
